2778124733a5b550f582b1ee525b2f2c982ccd6e
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2025-10-19 16:52:04 +0200

    Improve anyrun plugins and waybar stying

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -3,12 +3,12 @@ options="1 Sleep\n2 Shutdown\n3 Restart\n4 Logout\n5 Cancel"
 chosen=$(echo -e "$options" | wofi --no-custom-entry --hide-scroll --insensitive --dmenu --prompt "Power" --width 300 --lines=5 --sort-order="alphabetical")
 
 case $chosen in
-  "Shutdown") systemctl poweroff ;;
-  "Restart")   systemctl reboot ;;
-  "Logout")   niri msg action quit --skip-confirmation ;;
-  "Sleep")  systemctl suspend ;;
-  "Hibernate") systemctl hibernate ;;
-  "Lock")     swaylock ;;
-  "Exit")     exit 0 ;;
-  *)          exit 1 ;;
+  "2 Shutdown")  systemctl poweroff ;;
+  "3 Restart")   systemctl reboot ;;
+  "4 Logout")    niri msg action quit --skip-confirmation ;;
+  "1 Sleep")     systemctl suspend ;;
+  "Hibernate")   systemctl hibernate ;;
+  "Lock")        swaylock ;;
+  "5 Cancel")    exit 0 ;;
+  *)             exit 1 ;;
 esac
