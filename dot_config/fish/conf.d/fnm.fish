abd5c90bfeb8cae12660216569782868f50d2fb7
mgabor3141 <>
2025-11-13 11:01:19 +0100

    Add more brews

--- dot_config/fish/conf.d/fnm.fish
+++ dot_config/fish/conf.d/fnm.fish
@@ -1,1 +1,2 @@
-fnm env --use-on-cd --shell fish | source
+# See: https://github.com/Schniz/fnm/blob/master/docs/configuration.md
+fnm env --use-on-cd --corepack-enabled --version-file-strategy=recursive --resolve-engines --shell fish | source
