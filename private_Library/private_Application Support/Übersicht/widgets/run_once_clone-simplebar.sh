2af25d10035706777baac308f3121d2c42978997
mgabor3141 <@mgabor3141>
2025-11-23 20:12:10 +0100

    Remove simplebar

--- private_Library/private_Application Support/Übersicht/widgets/run_once_clone-simplebar.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-SIMPLE_BAR_DIR="$HOME/Library/Application Support/Übersicht/widgets/simple-bar"
-
-ls "$SIMPLE_BAR_DIR" >/dev/null 2>&1 || git clone https://github.com/Jean-Tinland/simple-bar "$SIMPLE_BAR_DIR"
-
-chmod +x "$SIMPLE_BAR_DIR/lib/scripts/init-aerospace.sh"
