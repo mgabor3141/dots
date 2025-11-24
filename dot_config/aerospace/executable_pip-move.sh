c9a356a221c7d8a53163eaa56fba836d99143a12
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2025-11-24 12:14:25 +0100

    Fix pip move

--- dot_config/aerospace/executable_pip-move.sh
+++ dot_config/aerospace/executable_pip-move.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 # Get current workspace
-current_workspace=$(aerospace list-workspaces --focused)
+current_workspace=$(/opt/homebrew/bin/aerospace list-workspaces --focused)
+
 # Move all PiP windows to current workspace (as defined by the regex)
-aerospace list-windows --all --json \
+/opt/homebrew/bin/aerospace list-windows --all --json \
   | jq -r '.[] | select(.["window-title"] | test("^(Picture[- ]in[- ]Picture|Meet - [a-z-]*)$")) | .["window-id"]' \
-  | xargs -r -n1 -I{} aerospace move-node-to-workspace --window-id {} "$current_workspace"
-done
+  | xargs -r -n1 -I{} /opt/homebrew/bin/aerospace move-node-to-workspace --window-id {} "$current_workspace"
