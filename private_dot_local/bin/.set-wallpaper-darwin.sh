71201e90d9f4627895c2ff90e3f3c95e98261e73
mgabor3141 <@mgabor3141>
2025-12-01 01:21:07 +0100

    Improve wallpaper setting

--- private_dot_local/bin/.set-wallpaper-darwin.sh
+++ private_dot_local/bin/.set-wallpaper-darwin.sh
@@ -1,1 +1,7 @@
+# Wait until the session is unlocked
+while [[ "$(stat -f '%Sg' /dev/console)" != "staff" ]]; do
+    sleep 5
+done
+
+# Set wallpaper
 osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$IMAGE\""
