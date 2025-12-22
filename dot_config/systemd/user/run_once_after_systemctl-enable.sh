d0163e65b099616db3f93a5ce2159a92b6a6db06
mgabor3141 <@mgabor3141>
2025-12-22 11:47:34 +0100

    Systemd: add script to enable some services by default

---
+++ dot_config/systemd/user/run_once_after_systemctl-enable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+trap 'echo "❌ Error on line $LINENO: $BASH_COMMAND" >&2' ERR
+set -Eeuo pipefail
+
+sudo systemctl enable --now coolercontrold.service
+
+sudo systemctl enable --now docker.socket
+sudo usermod -aG docker $USER
