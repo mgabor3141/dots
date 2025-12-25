b9652e70cd55cb749000af34e2c98e956be67bb6
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2025-12-25 19:48:43 +0100

    Add mute indicator overlay

---
+++ dot_config/waybar-mute-indicator/executable_handle-mute-events.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+trap 'echo "❌ Error on line $LINENO: $BASH_COMMAND" >&2' ERR
+set -Eeuo pipefail
+
+# Read pactl subscribe output line by line
+while IFS= read -r line; do
+    case "$line" in
+        *"Event 'change' on source #"*)
+            discord_id=$(niri msg --json windows | jq -r '.[] | select(.app_id == "vesktop") | .id')
+
+            if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q "Mute: yes"; then
+                # Muted
+                systemctl --user kill --signal SIGUSR1 waybar-mute-indicator.service
+                niri msg action set-window-urgent --id "$discord_id" || true
+            else
+                # Unmuted
+                systemctl --user kill --signal SIGUSR2 waybar-mute-indicator.service
+                niri msg action unset-window-urgent --id "$discord_id" || true
+            fi
+            ;;
+    esac
+done
