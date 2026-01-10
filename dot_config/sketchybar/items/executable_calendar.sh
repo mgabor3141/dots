0a9a1a40e61592b80d5894fea66f6161db288c50
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2026-01-10 14:51:01 +0100

    Sketchybar: make clock updates more frequent
    See: https://github.com/FelixKratz/SketchyBar/discussions/454

--- dot_config/sketchybar/items/executable_calendar.sh
+++ dot_config/sketchybar/items/executable_calendar.sh
@@ -2,12 +2,12 @@
 
 sketchybar --add item clock right \
   --set clock \
-  update_freq=30 \
+  update_freq=1 \
   script="$PLUGIN_DIR/clock.sh"
 
 sketchybar --add item calendar right \
   --set calendar \
-  update_freq=360 \
+  update_freq=30 \
   script="$PLUGIN_DIR/calendar.sh"
 
 # icon=􀧞
