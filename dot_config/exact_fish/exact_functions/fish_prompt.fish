17390eeee73f6758de06fe9caffdbb30d03d6b1e
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2026-01-28 13:14:35 +0100

    Improve fish prompt color handling and error status display
    - Refactor chevron color logic for clarity
    - Show background color on prompt if last command failed

--- dot_config/exact_fish/exact_functions/fish_prompt.fish
+++ dot_config/exact_fish/exact_functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt --description 'Write out the prompt'
         set -l normal (set_color normal)
         set -l cwd_color (set_color $fish_color_cwd)
         set -l vcs_color (set_color brpurple)
-        set -l highlight (set_color blue)
+        set -l chevron_normal (set_color blue)
         set -l chevron_color $cwd_color
 
         # Since we display the prompt on a new line allow the directory names to be longer.
@@ -13,17 +13,12 @@ function fish_prompt --description 'Write out the prompt'
         # Determine chevron symbol
         set -l suffix '❯'
         if functions -q fish_is_root_user; and fish_is_root_user
-                if set -q fish_color_cwd_root
-                        set cwd_color (set_color $fish_color_cwd_root)
-                end
                 set suffix '#'
         end
 
         # Determine chevron color based on history mode
         if test $__history_global_mode -eq 1
-                set chevron_color (set_color blue)
-        else
-                set chevron_color $cwd_color
+                set chevron_color $chevron_normal
         end
 
         # Mouse indicator
@@ -32,20 +27,25 @@ function fish_prompt --description 'Write out the prompt'
                 set mouse_indicator ' 🐁'
         end
 
+        # Blank line between commands
         echo ""
 
-        # Transient prompt (in scrollback)
         if contains -- --final-rendering $argv
-                echo -n -s $highlight $suffix ' ' $normal
-        # Full prompt (current)
+            # Prompt in scrollback
+                echo -n -s $chevron_normal $suffix ' ' $normal
         else
+            # Active prompt
                 if set -q SSH_CONNECTION
                         set_color blue
-                        echo -n "["(string replace -r '\.local$' '' (hostname))"] "
-                        set_color normal
+                        echo -n "["(string replace -r '\.local$' '' (hostname))"] " $normal
                 end
 
                 echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal $mouse_indicator
-                echo -n -s $chevron_color $suffix ' ' $normal
+
+                if test $last_status -ne 0
+                        set_color --background brred
+                end
+
+                echo -n -s $chevron_color $suffix $normal ' '
         end
 end
