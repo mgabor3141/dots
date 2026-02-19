532f42ec02eb243fcb4199dc131289d6c51acb7e
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2026-02-19 14:12:27 +0100

    Add Finnicky for browser routing on MacOS

---
+++ dot_finicky.ts
@@ -0,0 +1,19 @@
+import type { FinickyConfig } from "/Applications/Finicky.app/Contents/Resources/finicky.d.ts";
+
+export default {
+  options: {
+    checkForUpdates: false,
+    hideIcon: true,
+  },
+  defaultBrowser: "Zen",
+  handlers: [
+    {
+      // Open google.com and *.google.com urls in Google Chrome
+      match: [
+        "google.com/*", // match google.com urls
+        "*.google.com*", // also match google.com subdomains
+      ],
+      browser: "Google Chrome",
+    },
+  ],
+} satisfies FinickyConfig;
