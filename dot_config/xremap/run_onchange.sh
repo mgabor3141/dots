ac7d3f666c706a2e89e3d180b81a32909096361b
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2026-09-06 13:47:19 +0200

    Add sha256sum hash to xremap run_onchange

--- dot_config/xremap/run_onchange.sh
+++ dot_config/xremap/run_onchange.sh
@@ -3,5 +3,7 @@ trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
 set -Eeuo pipefail
 
 # Restart xremap when config changes
+# {{ include (joinPath .chezmoi.sourceDir "config.yml") | sha256sum }}
+
 systemctl --user daemon-reload
 systemctl --user restart xremap.service
