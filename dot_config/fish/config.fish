9ba213d9a96a797f2af49c259a156e62d3766c14
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2026-09-22 13:17:38 +0200

    Use icons=auto for eza aliases

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -102,10 +102,10 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons'  # long format
-alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias ls='eza -al --color=always --group-directories-first --icons=auto' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons=auto'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons=auto'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons=auto' # tree listing
 alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
 
 # Common use
